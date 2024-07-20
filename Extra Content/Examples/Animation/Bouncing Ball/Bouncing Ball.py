a93fb7554fa92a0be740727970aab66c11b7e54f
zrseah <161100014+zrseah@users.noreply.github.com>
2024-07-21 00:48:43 +0800

    Add files via upload

---
+++ Extra Content/Examples/Animation/Bouncing Ball/Bouncing Ball.py
@@ -0,0 +1,42 @@
+"""
+This is a worked example. This code is starter code; you should edit and run it to 
+solve the problem. You can click the blue show solution button on the left to see 
+the answer if you get too stuck or want to check your work!
+
+Note: The starter code for this example is the solution.
+"""
+
+from graphics import Canvas
+import time
+
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 450
+BALL_DIAMETER = 30
+INITIAL_VELOCITY = 5
+START_X = 0
+START_Y = 0
+DELAY = 0.001
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    x_velocity = INITIAL_VELOCITY
+    y_velocity = INITIAL_VELOCITY
+    ball_x = START_X
+    ball_y = START_Y
+    ball = canvas.create_oval(ball_x, ball_y,
+                              ball_x + BALL_DIAMETER,
+                              ball_y + BALL_DIAMETER,
+                              'blue')
+                              
+    while True:
+        if (ball_x < 0) or (ball_x + BALL_DIAMETER >= CANVAS_WIDTH):
+            x_velocity = -x_velocity
+        if (ball_y < 0) or (ball_y + BALL_DIAMETER >= CANVAS_HEIGHT):
+            y_velocity = -y_velocity
+        ball_x += x_velocity
+        ball_y += y_velocity
+        canvas.moveto(ball, ball_x, ball_y)
+        time.sleep(DELAY)
+
+if __name__ == '__main__':
+    main()
