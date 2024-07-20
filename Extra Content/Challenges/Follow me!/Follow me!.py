7f279a0582afc15c58497810f7bbb94742138471
zrseah <161100014+zrseah@users.noreply.github.com>
2024-07-21 00:31:38 +0800

    Add files via upload

---
+++ Extra Content/Challenges/Follow me!/Follow me!.py
@@ -0,0 +1,30 @@
+
+from graphics import Canvas
+import time
+
+CANVAS_SIZE = 400
+BALL_DIAMETER = 50
+PAUSE_TIME = 1/50
+
+def main():
+    canvas = Canvas(CANVAS_SIZE, CANVAS_SIZE)
+    ball = canvas.create_oval(
+        0, 0,
+        BALL_DIAMETER, 
+        BALL_DIAMETER,
+        'blue'
+    )
+    
+    while True:
+        mouse_x = canvas.get_mouse_x()
+        mouse_y = canvas.get_mouse_y()
+        
+        canvas.moveto(ball, mouse_x - BALL_DIAMETER/2, mouse_y - BALL_DIAMETER/2)
+        
+        time.sleep(PAUSE_TIME)
+        print("Mouse location: (" + str(mouse_x) + ", " + str(mouse_y) + ")")
+
+
+# There is no need to edit code beyond this point
+if __name__ == '__main__':
+    main()
